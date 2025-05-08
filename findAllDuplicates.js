57530eed759fe8d0412cd39f3947060674a06076
Kamil Jakóbczak <k_j1@vp.pl>
2025-05-08 16:51:29 +0200

    add findAllDuplicates

---
+++ findAllDuplicates.js
@@ -0,0 +1,24 @@
+// Frequency Counter - findAllDuplicates
+// Given an array of positive integers, some elements appear twice and others appear once. Find all the elements that appear twice in this array. Note that you can return the elements in any order.
+
+// findAllDuplicates([4,3,2,7,8,2,3,1]) // array with 2 and 3
+// findAllDuplicates([4, 3, 2, 1, 0]) // []
+// findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]) // array with 3, 2, and 1
+// Time Complexity - O(n)
+
+function findAllDuplicates(arr) {
+	const result = [];
+	const numbers = {};
+	for (const element of arr) {
+		if (numbers[element]) {
+			result.push(element);
+		} else {
+			numbers[element] = 1;
+		}
+	}
+	console.log(result);
+}
+
+findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1]); // array with 2 and 3
+findAllDuplicates([4, 3, 2, 1, 0]); // []
+findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]); // array with 3, 2, and 1
