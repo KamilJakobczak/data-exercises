a55a484207fbead28148762f33e6ee06154b5bd4
Kamil Jakóbczak <k_j1@vp.pl>
2025-05-10 12:31:24 +0200

    add alternative solutions to isSubsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -14,6 +14,7 @@
 // Space Complexity - O(1)
 
 function isSubsequence(str1, str2) {
+	// time O(n), space O(1)
 	let start = 0;
 	const textArr = str1.split('');
 	for (let index = 0; index < str2.length; index++) {
@@ -29,6 +30,23 @@ function isSubsequence(str1, str2) {
 		return true;
 	}
 	return false;
+
+	// iterative solution
+	// let i = 0;
+	// let j = 0;
+	// if (!str1) return true;
+	// while (j < str2.length) {
+	// 	if (str2[j] === str1[i]) i++;
+	// 	if (i === str1.length) return true;
+	// 	j++;
+	// }
+	// return false;
+
+	// recursive but not O(1) space Complexity
+	// if (str1.length === 0) return true;
+	// if (str2.length === 0) return false;
+	// if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+	// return isSubsequence(str1, str2.slice(1));
 }
 
 isSubsequence('hello', 'hello world'); // true
