d640621d024761cad9fa083ebe13cc7d86c84d17
Kamil Jakóbczak <k_j1@vp.pl>
2025-05-12 17:16:53 +0200

    add alternative solutions to findPair

--- findPair.js
+++ findPair.js
@@ -24,43 +24,77 @@
 // Time Complexity Requirement - O(n log n)
 
 function findPair(arr, result) {
-	if (arr.length < 1) {
-		console.log(false);
-		return false;
-	}
-
-	let start = 0;
-	let next = 1;
-	if (result === 0) {
-		let index = 0;
-		let hasDupe = false;
-		arr.sort((a, b) => a - b);
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i] === arr[i - 1]) {
-				if (!hasDupe) {
-					arr[index] = arr[i];
-					index++;
-					hasDupe = true;
-				} else {
-					hasDupe = false;
-				}
-			}
-		}
-		return hasDupe;
-	}
-	while (start < arr.length - 1) {
-		const deduction1 = arr[start] - arr[next];
-		const deduction2 = arr[next] - arr[start];
-		if (deduction1 === result || deduction2 === result) {
-			console.log(true);
-			return true;
-		} else {
-			next++;
-		}
-		start++;
-	}
-	console.log(false);
-	return false;
+	// MY SOLUTION
+	// if (arr.length < 1) {
+	// 	console.log(false);
+	// 	return false;
+	// }
+	// let start = 0;
+	// let next = 1;
+	// if (result === 0) {
+	// 	let index = 0;
+	// 	let hasDupe = false;
+	// 	arr.sort((a, b) => a - b);
+	// 	for (let i = 0; i < arr.length; i++) {
+	// 		if (arr[i] === arr[i - 1]) {
+	// 			if (!hasDupe) {
+	// 				arr[index] = arr[i];
+	// 				index++;
+	// 				hasDupe = true;
+	// 			} else {
+	// 				hasDupe = false;
+	// 			}
+	// 		}
+	// 	}
+	// 	return hasDupe;
+	// }
+	// while (start < arr.length - 1) {
+	// 	const deduction1 = arr[start] - arr[next];
+	// 	const deduction2 = arr[next] - arr[start];
+	// 	if (deduction1 === result || deduction2 === result) {
+	// 		console.log(true);
+	// 		return true;
+	// 	} else {
+	// 		next++;
+	// 	}
+	// 	start++;
+	// }
+	// console.log(false);
+	// return false;
+	// // O(n) Space + O(n) Time
+	// if (n === 0) {
+	// 	const set = new Set();
+	// 	for (let num of arr) {
+	// 		if (set.has(num)) {
+	// 			return true;
+	// 		}
+	// 		set.add(num);
+	// 	}
+	// 	return false;
+	// }
+	// const setValues = new Set(arr);
+	// for (let val of arr) {
+	// 	if (setValues.has(val + result) || setValues.has(val - result)) {
+	// 		return true;
+	// 	}
+	// }
+	// return false;
+	// O(1) space + O(n logn) time
+	// arr.sort((a, b) => a - b);
+	// let i = 0;
+	// let j = 0;
+	// while (i < arr.length && j < arr.length) {
+	// 	if (i !== j && Math.abs(arr[j] - arr[i]) === Math.abs(result)) {
+	// 		console.log(true);
+	// 		return true;
+	// 	} else if (arr[j] - arr[i] < result) {
+	// 		j++;
+	// 	} else {
+	// 		i++;
+	// 	}
+	// }
+	// console.log(false);
+	// return false;
 }
 
 findPair([6, 1, 4, 10, 2, 4], 2); // true
